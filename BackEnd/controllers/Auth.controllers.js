d2f934eaaab405badab0483658cc08b1016606dc
Varun Gupta <152791801+vaarun505@users.noreply.github.com>
2025-08-05 20:35:55 +0530

    Update Auth.controllers.js

--- BackEnd/controllers/Auth.controllers.js
+++ BackEnd/controllers/Auth.controllers.js
@@ -25,8 +25,8 @@ export const signUP = async (req, res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite: "strict",
-            secure:false
+            sameSite: "None",
+            secure:true
         })
 
         return res.status(201).json(user)
@@ -56,8 +56,8 @@ export const LogIn = async (req, res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite: "strict",
-            secure:false
+            sameSite: "None",
+            secure:true
         })
 
         return res.status(200).json(user)
